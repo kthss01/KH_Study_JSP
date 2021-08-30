08d3b6a138e55d8265f4f771c921a27ac0ef1b89
kthss01 <kthss01@naver.com>
2021-08-30 15:31:05 +0900

    JDBCTemplate 작성 연습

---
+++ JSP_Practice/src/com/kh/common/JDBCTemplate2.java
@@ -0,0 +1,74 @@
+package com.kh.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCTemplate2 {
+
+	public static Connection getConnection() {
+		Connection conn = null;
+		
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
+		String user = "server";
+		String password = "server";
+		
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, password);
+			conn.setAutoCommit(false);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	public static void close(Connection conn) {
+		try {
+			if (conn != null && !conn.isClosed())
+				conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void close(ResultSet rs) {
+		try {
+			if (rs != null && !rs.isAfterLast())
+				rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Statement stmt) {
+		try {
+			if (stmt != null && !stmt.isClosed())
+				stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void commit(Connection conn) {
+		try {
+			if (conn != null && !conn.isClosed())
+				conn.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void rollback(Connection conn) {
+		try {
+			if (conn != null && !conn.isClosed())
+				conn.rollback();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
